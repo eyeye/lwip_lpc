4808fbe3cf52fd7731eb0afa02dcc07da819cd08
Kevin Wells <kevin.wells@nxp.com>
2012-02-16 17:09:49 -0800

    Moved GNU specific path setup batch file Modifications to makefile for changes made in the last updates

---
+++ nxpcommon/examples/lpc177x_8x/ea1788/ea1788_common/gcc_addtoolpath.bat
@@ -0,0 +1,2 @@
+#EDIT this path to where your local directory is for the tools
+set PATH=\dev\git2\lpc177x_8x\makesection\tools;%PATH%
--- nxpcommon/examples/lpc177x_8x/ea1788/ea1788_httpraw_sa/gnu/addtoolpath.bat
+++
@@ -1,2 +0,0 @@
-
-set PATH=..\..\..\..\..\..\makesection\tools;%PATH%
